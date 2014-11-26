0664eba9811f6f99bffdeb9dee170d7b0603583c
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2014-11-26 08:38:35 +0300

    adding form validation mechanism

--- app/views/user/profileEdit.blade.php
+++ app/views/user/profileEdit.blade.php
@@ -4,6 +4,7 @@
     </div><!-- /.box-header -->
     <!-- form start -->
     <?php
+    $valid = "";
     if(Auth::user()->role == "company"){
         $valid = "disabled";
     }
@@ -13,43 +14,44 @@
         <div class="box-body">
             <div class="form-group">
                 <label for="firstname" class="col-sm-3">First Name</label>
-                <div class="col-sm-9"> <input type="text" class="form-control" id="firstname" name="firstname" value="{{ $user->firstname }}" placeholder="First Name"></div>
+                <div class="col-sm-9"> <input {{ $valid }} type="text" class="form-control" id="firstname" name="firstname" value="{{ $user->firstname }}" placeholder="First Name"></div>
             </div>
             <div class="form-group">
                 <label for="lastname" class="col-sm-3">Last Name</label>
-                <div class="col-sm-9"> <input type="text" class="form-control" id="lastname" name="lastname" value="{{ $user->lastname }}" placeholder="Last Name"></div>
+                <div class="col-sm-9"> <input {{ $valid }}  type="text" class="form-control" id="lastname" name="lastname" value="{{ $user->lastname }}" placeholder="Last Name"></div>
             </div>
             <div class="form-group">
                 <label for="middlename" class="col-sm-3">User Name</label>
-                <div class="col-sm-9"> <input type="text" class="form-control" id="middlename" name="username" value="{{ $user->username }}" placeholder="Middle Name"></div>
+                <div class="col-sm-9"> <input {{ $valid }}  type="text" class="form-control" id="middlename" name="username" value="{{ $user->username }}" placeholder="Middle Name"></div>
             </div>
 
 
             <div class="form-group">
                 <label for="email" class="col-sm-3">Email Address</label>
-                <div class="col-sm-9"> <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" placeholder="Email Address"></div>
+                <div class="col-sm-9"> <input {{ $valid }}  type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" placeholder="Email Address"></div>
             </div>
             <input type="hidden" name="role" value="{{ $user->role_id }}">
 
             <div class="form-group">
                 <label for="phone" class="col-sm-3">Phone Number</label>
-                <div class="col-sm-9"> <input type="text" class="form-control" id="phone" name="phone" value="{{ $user->phone }}" placeholder="Phone Number"></div>
+                <div class="col-sm-9"> <input {{ $valid }}  type="text" class="form-control" id="phone" name="phone" value="{{ $user->phone }}" placeholder="Phone Number"></div>
             </div>
-
+            @if(Auth::user()->role != 'company')
             <div class="form-group">
                 <label for="phone" class="col-sm-3">Password</label>
-                <div class="col-sm-9"> <input type="password" class="form-control" id="password" name="password" value="" ></div>
+                <div class="col-sm-9"> <input  type="password" class="form-control" id="password" name="password" value="" ></div>
             </div>
 
             <div class="form-group">
                 <label for="phone" class="col-sm-3">Re-enter Password</label>
                 <div class="col-sm-9"> <input type="password" class="form-control" id="password" name="re_enter_password" value="" placeholder=""></div>
             </div>
-        </div><!-- /.box-body -->
-        <div id="output"></div>
-        <div class="box-footer">
-            <button type="submit" class="btn btn-primary">Edit</button>
-        </div>
+            </div><!-- /.box-body -->
+            <div id="output"></div>
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Edit</button>
+            </div>
+            @endif
     </form>
 </div><script>
     $(document).ready(function (){
